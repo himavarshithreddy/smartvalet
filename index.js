754f858e6af7c509224f074b04248ae11bff7843
himavarshithreddy <himavarshithreddy@gmail.com>
2024-10-23 13:36:24 +0000

    path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const shortid = require('shortid');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
