e23db8a89f358fbd507eae3c5945c91228468141
himavarshithreddy <himavarshithreddy@gmail.com>
2024-10-25 14:08:18 +0000

    a

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(cors({ origin: '*', methods: ['GET', 'POST'] }));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
